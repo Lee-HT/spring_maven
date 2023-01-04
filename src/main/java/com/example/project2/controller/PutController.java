0ba827167f0717abc279e9193b728eca5d6bdf63
LEE-HT <12gusxkr@naver.com>
2023-01-04 11:28:01 +0900

    put delete

---
+++ src/main/java/com/example/project2/controller/PutController.java
@@ -0,0 +1,48 @@
+package com.example.project2.controller;
+
+import com.example.project2.dto.MemberDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/put")
+public class PutController {
+
+    @PutMapping("/hello")
+    public String putMethod() {
+        return "Hello world";
+    }
+
+    @PutMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping("/member1")
+    public String postMemberDto1(@RequestBody MemberDTO memberDTO){
+        return memberDTO.toString();
+    }
+
+    @PutMapping("/member2")
+    public MemberDTO postMemberDto2(@RequestBody MemberDTO memberDTO){
+        return memberDTO;
+    }
+
+    @PutMapping("/member3")
+    public ResponseEntity<MemberDTO> postMemberDto3(@RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDTO);  //httpstatus -> http code 응답
+    }
+
+}
