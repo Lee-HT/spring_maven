461946dd85404a635ac6d71215848c8301cef407
Lee-HT <12gusxkr@naver.com>
2023-01-10 22:16:34 +0900

    url success

--- src/main/java/com/example/project2/Project2Application.java
+++ src/main/java/com/example/project2/Project2Application.java
@@ -2,8 +2,10 @@ package com.example.project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //CreatedDate 어노테이션 작동용
 public class Project2Application {
 
 	public static void main(String[] args) {
