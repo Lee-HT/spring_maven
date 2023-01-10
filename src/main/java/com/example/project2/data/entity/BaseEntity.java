6cfd9c42504e9245c1103b3453dbf2a453c805aa
Lee-HT <12gusxkr@naver.com>
2023-01-10 20:51:46 +0900

    url success

--- src/main/java/com/example/project2/data/entity/BaseEntity.java
+++ src/main/java/com/example/project2/data/entity/BaseEntity.java
@@ -1,6 +1,5 @@
 package com.example.project2.data.entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
